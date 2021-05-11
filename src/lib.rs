7fe6110bae832544696f0d49537cb31b9e184136
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2021-05-11 22:28:37 +0700

    Added decode

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::char;
+use std::collections::HashMap;
 
 const BASE64ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 
@@ -41,6 +42,48 @@ pub fn encode(string: &str) -> String {
     result
 }
 
+pub fn decode(string: &str) -> String {
+    let chars: Vec<char> = string.chars().collect();
+    let alphabet: Vec<char> = BASE64ALPHABET.chars().collect();
+    let mut alphabet_num = HashMap::new();
+    let mut result = String::new();
+
+    for (i, chr) in alphabet.iter().enumerate() {
+        alphabet_num.insert(chr, i as u32);
+    }
+
+    let mut i = 0;
+
+    while i < chars.len() {
+        let mut bits: u32 = 0;
+        let bytes;
+
+        bits |= alphabet_num[&chars[i]] << 18;
+        bits |= alphabet_num[&chars[i + 1]] << 12;
+
+        if alphabet_num[&chars[i + 2]] == 64 {
+            bytes = bits.to_be_bytes()[1..2].to_vec();
+        } else if alphabet_num[&chars[i + 3]] == 64 {
+            bits |= alphabet_num[&chars[i + 2]] << 6;
+            bytes = bits.to_be_bytes()[1..3].to_vec();
+        } else {
+            bits |= alphabet_num[&chars[i + 2]] << 6;
+            bits |= alphabet_num[&chars[i + 3]];
+            bytes = bits.to_be_bytes()[1..4].to_vec();
+        }
+
+        let characters = std::str::from_utf8(&bytes);
+        match characters {
+            Ok(characters) => result.push_str(characters),
+            Err(_) => result.push_str(&String::new()),
+        }
+
+        i += 4;
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,6 +98,14 @@ mod tests {
             String::from(
                 "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdA=="
             )
+        );
+
+        assert_eq!(decode("SGkh"), String::from("Hi!"));
+        assert_eq!(decode("VGVzdA=="), String::from("Test"));
+        assert_eq!(decode("UnVzdA=="), String::from("Rust"));
+        assert_eq!(
+            decode("TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdA=="),
+            String::from("Lorem ipsum dolor sit amet, consectetur adipiscing elit")
         );
     }
 }
